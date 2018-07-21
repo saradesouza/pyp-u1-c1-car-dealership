75aae9e8d2d3a8c7522ea629d5167cf470031f18
saradesouza87 <saradesouza87@gmail.com>
2018-07-21 10:44:19 -0400

    um. derp. forgot to test against all test cases

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -10,7 +10,7 @@ class Vehicle(object):
     return self.base_price * self.SALE_PRICE_MULTIPLIER
 
   def purchase_price(self):
-    return (self.sale_price() - (self.PURCHASE_PRICE_MULTIPLIER * miles))
+    return (self.sale_price() - (self.PURCHASE_PRICE_MULTIPLIER * self.miles))
 
 class Car(Vehicle):
   SALE_PRICE_MULTIPLIER = 1.2
