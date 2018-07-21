9242834c709699f9696c898b83022567ba517323
saradesouza87 <saradesouza87@gmail.com>
2018-07-21 10:10:25 -0400

    stuck at test_vehicles, sale_price() missing 1 required positional argument: 'base_price'

--- dealership/vehicles.py
+++ dealership/vehicles.py
@@ -1,15 +1,27 @@
 class Vehicle(object):
-    def __init__(self, maker, model, year, base_price, miles):
-        pass
+  def __init__(self, maker, model, year, base_price, miles):
+    self.maker = maker
+    self.model = model
+    self.year = year
+    self.base_price = base_price
+    self.miles = miles
 
+  def sale_price(self, base_price):
+    sale_price = base_price * SALE_PRICE_MULTIPLIER 
+    return self.sale_price
+
+  def purchase_price(self, base_price):
+    purchase_price = base_price * PURCHASE_PRICE_MULTIPLIER 
+    return self.purchase_price
 
 class Car(Vehicle):
-    pass
-
+  SALE_PRICE_MULTIPLIER = 1.2
+  PURCHASE_PRICE_MULTIPLIER = 0.004
 
 class Motorcycle(Vehicle):
-    pass
-
+  SALE_PRICE_MULTIPLIER = 1.1
+  PURCHASE_PRICE_MULTIPLIER = 0.009
 
 class Truck(Vehicle):
-    pass
+  SALE_PRICE_MULTIPLIER = 1.6
+  PURCHASE_PRICE_MULTIPLIER = 0.02
